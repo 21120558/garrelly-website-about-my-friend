4763ab003d06956b8855ca3756186efad4f36e8e
hoaithi <93256403+hoaithi-rk@users.noreply.github.com>
2023-02-13 19:31:43 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,73 +1,166 @@
-
-// JavaScript Document
-$('#subscribeform').submit(function(){
-
-	var action = $(this).attr('action');
-
-		$("#mesaj").slideUp(750,function() {
-
-
-
-		$('#mesaj').hide();
-
-	$('#subsubmit')
-
-
-
-			.after('')
-
-
-
-			.attr('disabled','disabled');
-
-	$.post(action, {
-
-
-
-			email: $('#subemail').val()
-
-
-
-		},
-
-
-
-			function(data){
-
-
-
-				document.getElementById('mesaj').innerHTML = data;
-
-
-
-				$('#mesaj').slideDown('slow');
-
-
-
-				$('#subscribeform img.subscribe-loader').fadeOut('slow',function(){$(this).remove()});
-
-
-
-				$('#subsubmit').removeAttr('disabled');
-
-
-
-				if(data.match('success') != null) $('#subscribeform').slideUp('slow');
-
-			}
-
-
-
-		);
-
-
-
-		});
-
-
-
-		return false;
-
-
-
+
+
+// JavaScript Document
+
+$('#subscribeform').submit(function(){
+
+
+
+	var action = $(this).attr('action');
+
+
+
+		$("#mesaj").slideUp(750,function() {
+
+
+
+
+
+
+
+		$('#mesaj').hide();
+
+
+
+	$('#subsubmit')
+
+
+
+
+
+
+
+			.after('')
+
+
+
+
+
+
+
+			.attr('disabled','disabled');
+
+
+
+	$.post(action, {
+
+
+
+
+
+
+
+			email: $('#subemail').val()
+
+
+
+
+
+
+
+		},
+
+
+
+
+
+
+
+			function(data){
+
+
+
+
+
+
+
+				document.getElementById('mesaj').innerHTML = data;
+
+
+
+
+
+
+
+				$('#mesaj').slideDown('slow');
+
+
+
+
+
+
+
+				$('#subscribeform img.subscribe-loader').fadeOut('slow',function(){$(this).remove()});
+
+
+
+
+
+
+
+				$('#subsubmit').removeAttr('disabled');
+
+
+
+
+
+
+
+				if(data.match('success') != null) $('#subscribeform').slideUp('slow');
+
+
+
+			}
+
+
+
+
+
+
+
+		);
+
+
+
+
+
+
+
+		});
+
+
+
+
+
+
+
+		return false;
+
+
+
+
+
+
+
 	});
+
+	function myAudio()
+	{
+		document.getElementById("audio").play()
+	}
+
+
+	setTimeout(function () {
+		var click = document.getElementById("click-play");
+
+		disappearFunction(click)
+	}, 2000)
+
+
+	document.getElementById("heart").addEventListener("click", myAudio);
+
+	function disappearFunction(click) {
+		click.classList.toggle(
+			'disappear'
+		);
+	}
